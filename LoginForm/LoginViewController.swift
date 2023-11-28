6e299c5a63437a48203ee06449e75f700a2524d6
Darncol <60666804+Darncol@users.noreply.github.com>
2023-11-28 15:13:59 +0200

    Done

--- LoginForm/LoginViewController.swift
+++ LoginForm/LoginViewController.swift
@@ -10,51 +10,70 @@ import UIKit
 final class LoginViewController: UIViewController {
     
     @IBOutlet weak var userLoginTextField: UITextField!
-    
     @IBOutlet weak var userPasswordTextField: UITextField!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        userLoginTextField.delegate = self
-        userPasswordTextField.delegate = self
-    }
-    
-    
-    @IBAction func loginButtonTapped() {
-        performSegue(withIdentifier: SegueIdentifier.login, sender: nil)
-    }
+    private let user = "1"
+    private let password = "2"
     
     @IBAction func remindUserName() {
+        showAlert("User name is:", "1")
     }
     
     @IBAction func remindUserPassword() {
+        showAlert("User password is:", "2")
     }
-    
     
     @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
         userLoginTextField.text = ""
         userPasswordTextField.text = ""
     }
-    
 }
 
+// MARK: - UserInteraction
 extension LoginViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
+    
+    private func showAlert(
+        _ title: String,
+        _ message: String,
+        onOk: (() -> Void)? = nil
+    ) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            onOk?()
+        }
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
 }
 
-extension LoginViewController: UITextFieldDelegate {
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == userLoginTextField {
-            userPasswordTextField.becomeFirstResponder()
-        } else {
-            loginButtonTapped()
+// MARK: - SegueManagement
+extension LoginViewController {
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard  userLoginTextField.text == user,
+               userPasswordTextField.text == password
+        else {
+            showAlert("whoops", "Invalid login or password") {
+                self.userPasswordTextField.text = ""
+            }
+            return false
         }
         return true
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "welcomeScreen",
+              let destinationVC = segue.destination as? WelcomeViewController
+        else { return }
+        destinationVC.userName = userLoginTextField.text
+    }
 }
--- LoginForm/WelcomeViewController.swift
+++ LoginForm/WelcomeViewController.swift
@@ -8,14 +8,22 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var WelcomeLabel: UILabel!
+    
+    var userName: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.blue.cgColor, UIColor.white.cgColor]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.8, y: 0.8)
+        view.layer.insertSublayer(gradient, at: 0)
         
+        if let userName = userName {
+            WelcomeLabel.text = "Welcome, \(userName)!"
+        }
     }
-    
-
-  
-
 }
